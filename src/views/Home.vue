7be9ad9f10e3141193c2db1d7aa640bf24cf66ec
tianjia <tianjia@game.sina.com>
2021-08-04 11:24:08 +0800

    tj

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <contents/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import contents from '@/components/content.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+    contents
+  }
+}
+</script>
